88011fc2717aed9c5b58f0e1d689f83d8869a195
Preet Jaiswal <preetj886655@gmail.com>
2025-07-29 21:41:14 +0530

    Update project1.sql

--- project1.sql
+++ project1.sql
@@ -1,3 +1,19 @@
+CREATE DATABASE sql_project_retail_sales;
+
+CREATE TABLE retail_sales
+(
+    transactions_id INT PRIMARY KEY,
+    sale_date DATE,	
+    sale_time TIME,
+    customer_id INT,	
+    gender VARCHAR(10),
+    age INT,
+    category VARCHAR(35),
+    quantity INT,
+    price_per_unit FLOAT,	
+    cogs FLOAT,
+    total_sale FLOAT
+);
 use sql_project_retail_sales;
 SELECT * FROM retail_sales
 where transactions_id is null;
